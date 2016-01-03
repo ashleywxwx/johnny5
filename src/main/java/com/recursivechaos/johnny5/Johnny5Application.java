1127b94de5004e1ce2e28d371fe7cf37ab0f7be9
Andrew Bell <andrew@recursivechaos.com>
2016-01-03 14:45:11 -0600

    Adds consul registration.

--- src/main/java/com/recursivechaos/johnny5/Johnny5Application.java
+++ src/main/java/com/recursivechaos/johnny5/Johnny5Application.java
@@ -1,10 +1,14 @@
 package com.recursivechaos.johnny5;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableConfigurationProperties
 public class Johnny5Application {
 
